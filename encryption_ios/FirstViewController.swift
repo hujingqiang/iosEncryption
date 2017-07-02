ee0d4857d1e4818ae8da6052da2c4fb070415cd7
hujingqiang <hujingqiang@yahoo.com>
2017-07-02 20:14:45 +0800

    Initial Commit

---
+++ encryption_ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  encryption_ios
+//
+//  Created by jingqiang hu on 2/7/2017.
+//  Copyright © 2017 jingqiang hu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ encryption_ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  encryption_ios
+//
+//  Created by jingqiang hu on 2/7/2017.
+//  Copyright © 2017 jingqiang hu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

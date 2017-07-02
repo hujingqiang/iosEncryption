9ccafdce4af13077e51a2cbb6ef97bd56da1d4f2
hujingqiang <hujingqiang@yahoo.com>
2017-07-02 21:18:34 +0800

    add encryption class.

--- encryption_ios/FirstViewController.swift
+++ encryption_ios/FirstViewController.swift
@@ -13,6 +13,26 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let cryptoLib = CryptLib();
+        let plainText = "This is the text to be encrypted."
+        
+        let key = cryptoLib.sha256("my secret key", length: 32)!
+        let iv = "4NAfcTL5nWERGSLl"
+        // ((cryptoLib.generateRandomIV(11) as NSData).base64Encoding(withLineLength: 0) as NSString).substring(to: 16)
+        
+        print("key:\(key)")
+        print("iv:\(iv)")
+        let encryptedData=cryptoLib.encrypt(plainText.data(using: .utf8), key: key as String!, iv: iv)
+        //print("encryptedString:\(String(describing: NSString(data: encryptedString!, encoding: String.Encoding.utf8.rawValue)))")
+        print("encrypted string:\((encryptedData! as NSData).base64Encoding(withLineLength: 0)!)")
+        
+        let decryptedData = cryptoLib.decrypt(encryptedData, key: key as String!, iv: iv)
+        print("decrypted string:\(String(describing: NSString(data: decryptedData!, encoding: String.Encoding.utf8.rawValue)!))")
+        //print("encrypted string: \((decryptedData! as NSData).base64Encoding(withLineLength: 0))")
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ encryption_ios/NSData+Base64.h
@@ -0,0 +1,43 @@
+//
+//  NSData+Base64.h
+//  base64
+//
+//  Created by Matt Gallagher on 2009/06/03.
+//  Copyright 2009 Matt Gallagher. All rights reserved.
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty. In no event will the authors be held liable for any damages
+//  arising from the use of this software. Permission is granted to anyone to
+//  use this software for any purpose, including commercial applications, and to
+//  alter it and redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//  2. Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//  3. This notice may not be removed or altered from any source
+//     distribution.
+//
+
+#import <Foundation/Foundation.h>
+//
+//  NSData+Base64.m
+//
+// Derived from http://colloquy.info/project/browser/trunk/NSDataAdditions.h?rev=1576
+// Created by khammond on Mon Oct 29 2001.
+// Formatted by Timothy Hatcher on Sun Jul 4 2004.
+// Copyright (c) 2001 Kyle Hammond. All rights reserved.
+// Original development by Dave Winer.
+//
+
+@interface NSData (Base64)
+
++ (NSData *) dataWithBase64EncodedString:(NSString *) string;
+
+- (id) initWithBase64EncodedString:(NSString *) string;
+
+- (NSString *) base64EncodingWithLineLength:(unsigned int) lineLength;
+
+@end
